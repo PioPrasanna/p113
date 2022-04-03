27e836f8fe8bf75442504d7e4a192844db494d9a
PioPrasanna <91002032+PioPrasanna@users.noreply.github.com>
2022-04-03 10:50:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+    canvas=createCanvas(6500,480000090);
+    canvas.position(110,0);
+function draw(){
+  
+    circle(3, 30, 20);
+    ellipse(56, 57, 55, 32);
+    rect(30, 2, 55,35);
+    rect(30, 1, 25, 55, 130, 15, 10, 53);
+    
+}
+
